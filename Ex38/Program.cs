59e77808f33458395f6ca70aaed4a9cc76882298
Ilya Levshenkov <ivl1969@mail.ru>
2022-09-20 17:45:03 +0300

    add Ex38

---
+++ Ex38/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 38. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+// [3 7 22 2 78] -> 76
+
+//Создание и вывод массива.
+double[] GetArray(int size, int startValue, int endValue)
+{
+    double[] array = new double[size];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.NextDouble() + random.Next(startValue, endValue);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    return;
+}
+// Нахождение MAX числа.
+double MaxNumber(double[] array)
+{
+    double maxNumber = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (maxNumber < array[i])
+        {
+            maxNumber = array[i];
+        }
+
+    }
+    return maxNumber;
+}
+
+// Нахождение MIN числа.
+
+double MinNumber(double[] array)
+{
+    double minNumber = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (minNumber > array[i])
+        {
+            minNumber = array[i];
+        }
+
+    }
+    return minNumber;
+}
+
+try
+
+{
+    Console.WriteLine("Введите размер массива:");
+    int size = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите начало массива: ");
+    int start = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите конец массива: ");
+    int end = Convert.ToInt32(Console.ReadLine());
+
+    double[] array = GetArray(size, start, end);
+    Console.WriteLine($"Наш массив:");
+    PrintArray(array);
+    double max = MaxNumber(array);
+    double min = MinNumber(array);
+    double result = max - min;
+    Console.WriteLine("");
+    Console.WriteLine($"Разница между максимальным {max} и минимальным {min} числом массива = {result}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Входное значение имеет неверный формат. Ошибка: {ex.Message}");
+}
